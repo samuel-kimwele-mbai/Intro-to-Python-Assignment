412e931db476980fa743d1617d79326289ed7ebd
samuel-kimwele-mbai <mbaisamuel127@gmail.com>
2025-03-21 11:38:36 +0300

    Answers to week 1 assignemnet

---
+++ intro-to-python.py
@@ -0,0 +1,9 @@
+# Get user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform addition
+result = num1 + num2
+
+# Display the result
+print(f"{num1} + {num2} = {result}")
